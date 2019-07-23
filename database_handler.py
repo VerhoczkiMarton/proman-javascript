af70cb449ad488b2931b322cd1c834d583a792a8
Márton Verhóczki <verhoczki.marci@gmail.com>
2019-07-23 15:24:02 +0200

    Hotfix

--- database_handler.py
+++ database_handler.py
@@ -17,11 +17,12 @@ def get_cards_for_board(cursor, board_id):
 
 
 def get_latest_board_id():
-    return get_boards()[-1['id']]
+    return get_boards()[-1]['id']
+
 
 @connection_handler
 def add_board(cursor):
-    board_title = 'Board'+str(get_latest_board_id()[-1['id']]+1)
+    board_title = 'Board'+str(get_latest_board_id()+1)
     cursor.execute("""
     INSERT INTO board(title) VALUES (%(board_title)s) 
     """, {'board_title': board_title})
