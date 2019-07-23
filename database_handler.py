e4c03cd31fa721c8d35b58ad3c042a4388387ef9
Fanni <nagy.fanni.5@gmail.com>
2019-07-23 15:14:14 +0200

    add add_board

--- database_handler.py
+++ database_handler.py
@@ -14,3 +14,15 @@ def get_cards_for_board(cursor, board_id):
     SELECT * FROM card WHERE card.board_id = %(board_id)s
     """, {'board_id': board_id})
     return cursor.fetchall()
+
+
+def get_latest_board_id():
+    return get_boards()[-1['id']]
+
+@connection_handler
+def add_board(cursor):
+    board_title = 'Board'+str(get_latest_board_id()[-1['id']]+1)
+    cursor.execute("""
+    INSERT INTO board(title) VALUES (%(board_title)s) 
+    """, {'board_title': board_title})
+
